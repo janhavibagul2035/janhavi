fb9972d33efa04b22b0453bf4d7d66346e55f8de
janhavibagul2035 <58584874+janhavibagul2035@users.noreply.github.com>
2019-12-06 12:03:39 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the no");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("%d id not prime no",num);
+}
+else
+printf("%d is prime no",num);
+}
